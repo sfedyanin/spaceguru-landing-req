4104a756dc2ec574b753af10fb4b214fe41e4335
sfedyanin <sergey.fedyanin.dev@gmail.com>
2020-07-08 03:39:05 +0400

    Minor change on Menu.pug for delivery 2

--- routes/menu.js
+++ routes/menu.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	res.render('menu', {
 		title: 'Menus',
-		headerType: 'backbg'
+		headerType: 'backbg',
+		custom: 'custom_height'
 	});
 });
 
